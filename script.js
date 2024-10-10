526cf1ac251d8bdebd64769b8ae773e055cbac94
Manthan Thakor <thakormanthan849@gmail.com>
2024-10-10 23:40:13 +0530

    change 10

--- script.js
+++ script.js
@@ -2,3 +2,23 @@ function toggleMenu() {
   const nav = document.getElementById("nav-links");
   nav.classList.toggle("active");
 }
+
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  const backToTopBtn = document.getElementById("backToTopBtn");
+  if (
+    document.body.scrollTop > 300 ||
+    document.documentElement.scrollTop > 300
+  ) {
+    backToTopBtn.style.display = "block";
+  } else {
+    backToTopBtn.style.display = "none";
+  }
+}
+
+function scrollToTop() {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+}
